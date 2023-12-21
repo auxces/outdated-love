f6dd90e3fa30d0b1ea8fecd90d99049f73caf02f
Mark Adakwaah-Yiadom <135479787+coenum@users.noreply.github.com>
2023-12-21 11:49:36 -0500

    index and js update

--- shine.js
+++ shine.js
@@ -1,15 +1,26 @@
 let x = document.getElementById("shine");
-let y = document.getElementById("peace")
+let y = document.getElementById("peace");
+let z = 0;
 
 function playAudio() {
     x.volume = 0.1;
+    y.volume = 0.1;
     x.play();
     y.play();
   }
   
   function pauseAudio() {
-    x.pause();
-    y.pause();
+    if (z == 0){
+      x.pause();
+      y.pause();
+      z = 1;
+    }
+    else{
+      x.play();
+      y.play();
+      z = 0;
+    }
+    
   }
 
   function muteAudio(){
@@ -17,5 +28,13 @@ function playAudio() {
       x.volume = 0;
     } else {
       x.volume = 0.1;
+    }
+  }
+
+  function muteVideo(){
+    if (y.volume == 0.1){
+      y.volume = 0;
+    } else {
+      y.volume = 0.1;
     }
   }
