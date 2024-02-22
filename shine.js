06a65f62ff91b1963d92db0119f0ada5786e8116
Mark Adakwaah-Yiadom <135479787+coehix@users.noreply.github.com>
2024-02-22 17:14:23 -0500

    hidebackground function created, new pfp, name, tag, updated markers, new background

--- shine.js
+++ shine.js
@@ -1,10 +1,11 @@
 let x = document.getElementById("shine");
 let y = document.getElementById("peace");
 let z = 0;
+let vol = 1
 
 function playAudio() {
-    x.volume = 0.1;
-    y.volume = 0.1;
+    x.volume = vol;
+    y.volume = vol;
     x.play();
     y.play();
   }
@@ -24,20 +25,43 @@ function playAudio() {
   }
 
   function muteAudio(){
-    if (x.volume == 0.1){
+    if (x.volume > 0){
       x.volume = 0;
     } else {
-      x.volume = 0.1;
+      x.volume = vol;
     }
   }
 
   function muteVideo(){
-    if (y.volume == 0.1){
+    if (y.volume > 0){
       y.volume = 0;
     } else {
-      y.volume = 0.1;
+      y.volume = vol;
     }
   }
-  
+
   //disable right click just cause y not 
- // document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+let main = document.getElementById("maincontent");
+let gif = document.getElementById("toxic");
+let quote = document.getElementById("angels");
+let hidden = 0;
+
+
+function hideForeground(){
+  if(hidden == 0){
+    main.style.display = 'none';
+    gif.style.display = 'none';
+    quote.style.display = 'none';
+    y.style.opacity = "1";
+    hidden = 1
+  }
+  else{
+    main.style.display = 'flex';
+    gif.style.display = 'inline';
+    quote.style.display = 'flex';
+    y.style.opacity = "0.27";
+    hidden = 0
+  }
+}
