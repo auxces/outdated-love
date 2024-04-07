14a312de27fc2e19ac6b2b434145971a2b469b10
Mark Adakwaah-Yiadom <135479787+coehix@users.noreply.github.com>
2024-04-07 16:08:23 -0400

    intoxicated v2

--- shine.js
+++ shine.js
@@ -3,42 +3,54 @@ let y = document.getElementById("peace");
 let z = 0;
 let vol = 1
 
+function starter(){
+  x.volume = vol;
+  y.volume = vol;
+  x.play();
+  y.play();
+  document.getElementById('maincontent').className = 'slidein';
+  document.getElementById('angels').className = 'slidein';
+  document.getElementById('toxic').className = 'revslidein';
+  document.getElementById('sins').className = 'revslidein';
+}
+
 function playAudio() {
-    x.volume = vol;
-    y.volume = vol;
+  x.volume = vol;
+  y.volume = vol;
+  x.play();
+  y.play();
+}
+
+
+function pauseAudio() {
+  if (z == 0){
+    x.pause();
+    y.pause();
+    z = 1;
+  }
+  else{
     x.play();
     y.play();
+    z = 0;
   }
   
-  function pauseAudio() {
-    if (z == 0){
-      x.pause();
-      y.pause();
-      z = 1;
-    }
-    else{
-      x.play();
-      y.play();
-      z = 0;
-    }
-    
-  }
+}
 
-  function muteAudio(){
-    if (x.volume > 0){
-      x.volume = 0;
-    } else {
-      x.volume = vol;
-    }
+function muteAudio(){
+  if (x.volume > 0){
+    x.volume = 0;
+  } else {
+    x.volume = vol;
   }
+}
 
-  function muteVideo(){
-    if (y.volume > 0){
-      y.volume = 0;
-    } else {
-      y.volume = vol;
-    }
+function muteVideo(){
+  if (y.volume > 0){
+    y.volume = 0;
+  } else {
+    y.volume = vol;
   }
+}
 
   //disable right click just cause y not 
 document.addEventListener('contextmenu', event => event.preventDefault());
